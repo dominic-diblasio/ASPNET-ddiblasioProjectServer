a09fc073d4ec516b0a275e8085ae0de93971b139
dominic-diblasio <dcd10883@ECS1538-02>
2025-02-21 18:16:29 -0800

    Add project files.

---
+++ ddiblasioModel/City.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ddiblasioModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    public int Population { get; set; }
+
+    [Column("CountryID")]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
